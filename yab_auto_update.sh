8f8a8b8392790b97e1e87e68da018ca16d900c2f
M21 <141174453+habeshaone@users.noreply.github.com>
2023-08-23 12:30:38 -0400

    Add files via upload

---
+++ yab_auto_update.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#!/bin/bash
+
+exit_status() {
+    if [ $? -eq 0 ]
+        then
+                echo
+                echo "-----------------------------------------"
+                echo "*   Command Execution Was Successful.   *"
+                echo "-----------------------------------------"
+                echo
+        else
+                echo
+                echo "-------------------------------------------------------"
+                echo "     [Error] Process Command Execution Has Failed!     "
+                echo "-------------------------------------------------------"
+                echo
+
+        read -p "The last command has exited with an error. Exit script? (yes/no)" answer
+                if [ "$answer" == "yes" ]
+                    then
+                        exit 1
+                fi 
+    fi 
+}
+
+start() {
+            echo
+            echo "--------------------------------------------"
+            echo "***>>> Upgrading The Operating System <<<***"
+            echo "--------------------------------------------"
+            echo
+}
+
+upgrade() {
+            sudo apt update;
+            exit_status
+
+            sudo apt upgrade;
+            exit_status
+}
+
+cleanUp() {
+            sudo apt autoremove;
+            exit_status
+}
+
+exitUpdate() {
+
+            echo
+            echo "-------------------------------------------------------"
+            echo ">>>>   Operating System Update Has Been Completed  <<<<"
+            echo "-------------------------------------------------------"
+            echo
+        exit
+}
+
+#calls the functions
+start
+upgrade
+cleanUp
+exitUpdate
+
